746efd83228673d3edb0a8fe1d934c5862a7580e
Jason Johnson <jason.r.johnson86@gmail.com>
2024-03-18 20:30:23 -0500

    Added Trainer import

--- Task1.py
+++ Task1.py
@@ -3,9 +3,11 @@ from transformers import AutoTokenizer
 from transformers import AutoModelForSequenceClassification
 from transformers import AutoModelForCausalLM, AutoTokenizer
 from transformers import TrainingArguments, Trainer
+import torch
 import numpy as np
 import evaluate
 
+max_length = 256
 #test
 def compute_metrics(eval_pred):
     logits, labels = eval_pred
@@ -13,23 +15,25 @@ def compute_metrics(eval_pred):
     return metric.compute(predictions=predictions, references=labels)
 
 def tokenize_function(examples):
-    return tokenizer(examples["text"], truncation=True)
+    tokenizer.pad_token = tokenizer.eos_token
+    return tokenizer(examples["text"], truncation=True, padding='max_length', max_length=max_length)
 
 modelName = "microsoft/phi-2"
 dataset = load_dataset("flytech/python-codes-25k")
 
-tokenizer = AutoTokenizer.from_pretrained(modelName, trust_remote_code=True)
+tokenizer = AutoTokenizer.from_pretrained(modelName, trust_remote_code=True, torch_dtype=torch.float32)
 tokenized_datasets = dataset.map(tokenize_function, batched=True)
 
-model = AutoModelForCausalLM.from_pretrained(modelName, torch_dtype="auto", trust_remote_code=True)
+model = AutoModelForCausalLM.from_pretrained(modelName, trust_remote_code=True, torch_dtype=torch.float32)
+# model = AutoModelForCausalLM.from_pretrained(modelName, torch_dtype="auto", trust_remote_code=True, torch_dtype=torch.float32)
 # model = AutoModelForSequenceClassification.from_pretrained(modelName, num_labels=4)
 training_args = TrainingArguments(output_dir="test_trainer",evaluation_strategy="epoch")
 
 trainer = Trainer(
     model=model,
     args=training_args,
-    train_dataset=tokenized_datasets["train"][0:100],
-    eval_dataset=tokenized_datasets["train"][101:200],
+    train_dataset=tokenized_datasets["train"],
+    eval_dataset=tokenized_datasets["train"],
     compute_metrics=compute_metrics,
 )
 
