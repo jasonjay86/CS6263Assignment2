5e7f1593b7962afeadb4bf1724fe90e2c39b2b42
Jason Johnson <jason.r.johnson86@gmail.com>
2024-03-18 19:05:38 -0500

    updated for phi2

--- Task1.py
+++ Task1.py
@@ -1,6 +1,7 @@
 from datasets import load_dataset
 from transformers import AutoTokenizer
 from transformers import AutoModelForSequenceClassification
+from transformers import AutoModelForCausalLM, AutoTokenizer
 from transformers import TrainingArguments
 import numpy as np
 import evaluate
@@ -12,15 +13,16 @@ def compute_metrics(eval_pred):
     return metric.compute(predictions=predictions, references=labels)
 
 def tokenize_function(examples):
-    return tokenizer(examples["text"], padding="max_length", truncation=True)
+    return tokenizer(examples["text"], truncation=True)
 
 modelName = "microsoft/phi-2"
 dataset = load_dataset("flytech/python-codes-25k")
 
-tokenizer = AutoTokenizer.from_pretrained(modelName)
+tokenizer = AutoTokenizer.from_pretrained(modelName, trust_remote_code=True)
 tokenized_datasets = dataset.map(tokenize_function, batched=True)
 
-model = AutoModelForSequenceClassification.from_pretrained(modelName, num_labels=4)
+model = AutoModelForCausalLM.from_pretrained(modelName, torch_dtype="auto", trust_remote_code=True)
+# model = AutoModelForSequenceClassification.from_pretrained(modelName, num_labels=4)
 training_args = TrainingArguments(output_dir="test_trainer",evaluation_strategy="epoch")
 
 trainer = Trainer(
