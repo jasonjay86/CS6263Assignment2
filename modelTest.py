061f87c68707b7a2d45f83ca4da2df57b65015da
Jason Johnson <jason.r.johnson86@gmail.com>
2024-03-20 15:42:23 -0500

    Working Phi model

--- modelTest.py
+++ modelTest.py
@@ -1,13 +1,17 @@
 from transformers import (
     AutoModelForCausalLM,
      AutoTokenizer)
+from peft import PeftModel, PeftConfig
 
-modelpath = "./results"
-testPrompt = "Write some code to count to 5"
-
+modelpath = "./FTPhi2_dev"
+testPrompt = "Edit the following Python program to implement try and except a = 10 b = 0 c = a/b"
 model = AutoModelForCausalLM.from_pretrained(modelpath)
+model = PeftModel.from_pretrained(model, modelpath)
+# model = AutoModelForCausalLM.from_pretrained(modelpath)
 tokenizer = AutoTokenizer.from_pretrained(modelpath)
 
 input = tokenizer(testPrompt, return_tensors="pt").input_ids
-outputs = model.generate(input, max_length = 200)
-print(tokenizer.decode(outputs[0], skip_special_tokens=True))
+outputs = model.generate(input, max_length = 100)
+text = tokenizer.decode(outputs[0], skip_special_tokens=True)
+
+print(text)
