6d684e0e943fe2d44a90a8d5be64c7a5c0e29ae1
Jason Johnson <jason.r.johnson86@gmail.com>
2024-03-23 15:12:21 -0500

    Updated Evaluation for hyperparameters

--- Evaluation.py
+++ Evaluation.py
@@ -8,15 +8,70 @@ from codebleu import calc_codebleu
 from rouge import Rouge
 from bert_score import score
 
+def getOutput(tokenizer,model,testPrompt,hparam,size=0):
+    input = tokenizer(testPrompt, return_tensors="pt").input_ids
+
+    if hparam == "vanilla":
+        outputs = model.generate(input, max_length = 450)
+    elif hparam == "topK":
+        outputs = model.generate(input,
+                                 max_length = 450,
+                                 do_sample=True,
+                                 top_k=size)
+    elif hparam == "beam":
+        outputs = model.generate(input,
+                                 max_length = 450,
+                                 num_beams=size,
+                                 early_stopping=True)
+    elif hparam == "temp":
+         outputs = model.generate(input,
+                                 max_length = 450,
+                                 do_sample=True,
+                                 top_k=0,
+                                 temperature = size)
+
+    text = tokenizer.decode(outputs[0], skip_special_tokens=True)
+    return text
+
+
 modelList = [
-    "./Llama",
-    "./FTPhi2_dev",
-    "./Mistral"
+    "./Llama"#,
+    # "./FTPhi2_dev",
+    # "./Mistral"
 ]
+
+outputType = [
+    "vanilla",
+    "topK",
+    "beam",
+    "temp"
+]
+
+topKsize = [
+    # 2,
+    # 4,
+    # 6,
+    8
+]
+
+beamsize = [
+    # 2,
+    # 3,
+    # 5,
+    25
+]
+
+tempSize = [
+    # .1,
+    # .25,
+    # .5,
+    .75
+]
+
 datapath = "flytech/python-codes-25k"
 
 dataset = load_dataset("flytech/python-codes-25k", split='train')
-numInputs = 20
+numInputs = 1
 
 randrows = []
 for i in range(numInputs):
@@ -30,40 +85,55 @@ for modelpath in modelList:
     model = PeftModel.from_pretrained(model, modelpath)
     tokenizer = AutoTokenizer.from_pretrained(modelpath)
 
-    referencelist = []
-    predictionlist = []
-
-    for i in range(numInputs):
-        testPrompt = dataset[i]["instruction"]
-        referencelist.append(dataset[i]["output"])
-        input = tokenizer(testPrompt, return_tensors="pt").input_ids
-        outputs = model.generate(input, max_length = 450)
-        text = tokenizer.decode(outputs[0], skip_special_tokens=True)
-        predictionlist.append(text)
     
-    print("Results for " + str(modelpath))
-    print('-' * 80)
-    ##codebleu##
-    codebleuResult = calc_codebleu(referencelist, predictionlist, lang="python", weights=(0.25, 0.25, 0.25, 0.25), tokenizer=None)
-    print("CodeBleu Scrore: " + str(codebleuResult["codebleu"]))
-    ##rouge##
-    rouge = Rouge()
-    scores = rouge.get_scores(predictionlist, referencelist, avg=True)
-    print("Rouge-L score: " + str(scores["rouge-l"]))
-    ##BERTscore##
-    P, R, F1 = score(predictionlist, referencelist, lang="en", verbose=True)
-    print("BERTScore:")
-    print(P, R, F1)
 
-    print('-' * 80)
-    print("")
+    for hparam in outputType:
+        sizes = []
+        if hparam == "vanilla":
+            sizes = 1
+        elif hparam == "topK":
+            sizes = topKsize
+        elif hparam == "beam":
+            sizes == beamsize
+        elif hparam == "temp":
+            sizes == tempSize
 
-    print("For Human Evaluation on : " + str(modelpath))
-    for i in range(numInputs):
-        print("Instruction " + str(i))
-        print(dataset[i]["instruction"])
-        print(str(modelpath) + " output:")
-        print(predictionlist[i])
-        print('-' * 80)
+        for size in sizes:
+            referencelist = []
+            predictionlist = []
+            for i in range(numInputs):
+                testPrompt = dataset[i]["instruction"]
+                
+                text = getOutput(tokenizer,model,testPrompt,hparam,size)
+                
+                referencelist.append(dataset[i]["output"])
+                predictionlist.append(text)
+            
+            print("Results for " + str(modelpath)+ "... output type: "+ str(hparam)+ "size = "+ size)
+            print('-' * 80)
+            ##codebleu##
+            codebleuResult = calc_codebleu(referencelist, predictionlist, lang="python", weights=(0.25, 0.25, 0.25, 0.25), tokenizer=None)
+            print("CodeBleu Scrore: " + str(codebleuResult["codebleu"]))
+            ##rouge##
+            rouge = Rouge()
+            scores = rouge.get_scores(predictionlist, referencelist, avg=True)
+            print("Rouge-L score: " + str(scores["rouge-l"]))
+            ##BERTscore##
+            P, R, F1 = score(predictionlist, referencelist, lang="en", verbose=True)
+            print("BERTScore:")
+            print(P, R, F1)
+
+            print('-' * 80)
+            print("")
+
+            print("For Human Evaluation on : " + str(modelpath))
+            for i in range(numInputs):
+                print("Instruction " + str(i))
+                
+                print(dataset[i]["instruction"])
+                print("***")
+                print(str(modelpath) + " output:")
+                print(predictionlist[i])
+                print('-' * 80)
 
 
