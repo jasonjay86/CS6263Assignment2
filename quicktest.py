66048b95c5eadb51fcd5d24a98a3cd7b0499dc8d
Jason Johnson <jason.r.johnson86@gmail.com>
2024-04-07 17:00:09 +0000

    deletin unnecessary files

--- quicktest.py
+++
@@ -1,8 +0,0 @@
-def convertToBinary(number):
-    binary = ''
-    while number > 0:
-        binary = bin(number)[2:]
-        number = number // 2
-    return binary
-
-print(convertToBinary(155))
